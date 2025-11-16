4ed9305728663fc3823ec0c26d8b60892a272397
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2025-11-16 20:49:23 +0000

    fix issues with summoning circle rendering

--- src/main/java/net/smileycorp/magiadaemonica/client/rituals/renderers/SummoningCircleRenderer.java
+++ src/main/java/net/smileycorp/magiadaemonica/client/rituals/renderers/SummoningCircleRenderer.java
@@ -22,15 +22,19 @@ public class SummoningCircleRenderer implements RitualRenderer<SummoningCircle> 
         ResourceLocation name = ritual.getName();
         if (name == null) return;
         Minecraft mc = Minecraft.getMinecraft();
-        mc.getTextureManager().bindTexture(new ResourceLocation(name.getResourceDomain(), "textures/summoning_circles/" + name.getResourcePath() + ".png"));
+        //setup lighting
         RenderHelper.enableStandardItemLighting();
         GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
         GlStateManager.enableTexture2D();
+        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
+
+        //summoning cirlce
         int i = mc.world.getCombinedLight(ritual.getCenter(), 0);
         int j = i % 65536;
         int k = i / 65536;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
         GlStateManager.color(1, 1, 1, 1);
+        mc.getTextureManager().bindTexture(new ResourceLocation(name.getResourceDomain(), "textures/summoning_circles/" + name.getResourcePath() + ".png"));
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder buffer = tessellator.getBuffer();
         buffer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR_NORMAL);
@@ -39,35 +43,29 @@ public class SummoningCircleRenderer implements RitualRenderer<SummoningCircle> 
         buffer.pos(width, 0.01, height).tex(1, 1).color(255, 255, 255, 255).normal(0, 1, 0).endVertex();
         buffer.pos(width, 0.01, 0).tex(1, 0).color(255, 255, 255, 255).normal(0, 1, 0).endVertex();
         tessellator.draw();
-        float offsetX = width * 0.5f;
-        float offsetZ = height * 0.5f;
+
+        //candles
+        float offsetX = width * 0.5f - 0.5f;
+        float offsetZ = height * 0.5f - 0.5f;
+        WorldClient world = mc.world;
+        BlockRendererDispatcher dispatcher = mc.getBlockRendererDispatcher();
         mc.getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-        for (float[] candle : ritual.getCandles()) renderCandle(mc, ritual, candle[0] + offsetX - 0.5, candle[1] + offsetZ - 0.5);
+        buffer.begin(7, DefaultVertexFormats.BLOCK);
+        for (float[] candle : ritual.getCandles()) {
+            BlockPos pos = ritual.getPos().add(x, 0, z);
+            IBlockState state = world.getBlockState(pos);
+            boolean lit = false;
+            if (state.getBlock() == DaemonicaBlocks.CHALK_LINE) lit = state.getValue(BlockChalkLine.CANDLE) == BlockChalkLine.Candle.LIT;
+            IBlockState renderState = DaemonicaBlocks.SCENTED_CANDLE.getDefaultState().withProperty(BlockScentedCandle.LIT, lit);
+            buffer.setTranslation(candle[0] + offsetX - pos.getX(), -pos.getY(), candle[1] + offsetZ - pos.getZ());
+            dispatcher.getBlockModelRenderer().renderModel(world, dispatcher.getModelForState(renderState), renderState,  pos, buffer, false, MathHelper.getPositionRandom(pos));
+            buffer.setTranslation(0, 0, 0);
+        }
+        tessellator.draw();
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
         GlStateManager.disableTexture2D();
         GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
     }
-
-    private void renderCandle(Minecraft mc, IRitual ritual, double x, double z) {
-        WorldClient world = mc.world;
-        BlockPos pos = ritual.getPos().add(x, 0, z);
-        IBlockState state = world.getBlockState(pos);
-        boolean lit = false;
-        if (state.getBlock() == DaemonicaBlocks.CHALK_LINE) lit = state.getValue(BlockChalkLine.CANDLE) == BlockChalkLine.Candle.LIT;
-        int i = world.getCombinedLight(pos, 0);
-        int j = i % 65536;
-        int k = i / 65536;
-        GlStateManager.pushMatrix();
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)j, (float)k);
-        BlockRendererDispatcher dispatcher = mc.getBlockRendererDispatcher();
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder buffer = tessellator.getBuffer();
-        IBlockState state1 = DaemonicaBlocks.SCENTED_CANDLE.getDefaultState().withProperty(BlockScentedCandle.LIT, lit);
-        buffer.begin(7, DefaultVertexFormats.BLOCK);
-        GlStateManager.translate(x, 0 , z);
-        dispatcher.getBlockModelRenderer().renderModel(world, dispatcher.getModelForState(state1), state1, BlockPos.ORIGIN, buffer, false, MathHelper.getPositionRandom(pos));
-        tessellator.draw();
-        GlStateManager.popMatrix();
-    }
-
 
 }
