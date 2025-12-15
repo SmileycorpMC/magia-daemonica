b4b6ffa26d2e8cbc84404dcd2121778349bf03a5
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2025-12-15 02:09:28 +0000

    fix health cost bug

--- src/main/java/net/smileycorp/magiadaemonica/common/demons/contracts/costs/HealthCost.java
+++ src/main/java/net/smileycorp/magiadaemonica/common/demons/contracts/costs/HealthCost.java
@@ -4,9 +4,12 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
+import net.smileycorp.atlas.api.util.MathUtils;
 import net.smileycorp.magiadaemonica.common.Constants;
 import net.smileycorp.magiadaemonica.common.demons.Demon;
 import net.smileycorp.magiadaemonica.common.demons.contracts.ContractsUtils;
+import software.bernie.geckolib3.core.util.MathUtil;
 
 public class HealthCost implements Cost {
 
@@ -50,7 +53,7 @@ public class HealthCost implements Cost {
     }
 
     public static HealthCost generate(Demon demon, EntityPlayer player, int tier) {
-        return new HealthCost(0.5f * (int)(tier + 1 - (player.getRNG().nextInt(tier) * 0.5f)));
+        return new HealthCost(MathHelper.clamp(0.5f * (int)(tier + 1 - (player.getRNG().nextInt(tier) * 0.5f)), 1, 10));
     }
 
 }
