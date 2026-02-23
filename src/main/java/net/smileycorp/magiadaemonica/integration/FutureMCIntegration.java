62a1f1279a9d6c54ba45ad3d4a26718c2eb58a5d
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-02-23 23:55:58 +0000

    add futuremc honeycomb to wax oredict if deeper depths isn't installed

--- src/main/java/net/smileycorp/magiadaemonica/integration/FutureMCIntegration.java
+++ src/main/java/net/smileycorp/magiadaemonica/integration/FutureMCIntegration.java
@@ -3,12 +3,15 @@ package net.smileycorp.magiadaemonica.integration;
 
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.oredict.OreDictionary;
 import net.smileycorp.magiadaemonica.common.blocks.DaemonicaBlocks;
 import thedarkcolour.futuremc.recipe.stonecutter.StonecutterRecipes;
+import thedarkcolour.futuremc.registry.FItems;
 
 public class FutureMCIntegration {
 
     public static void registerRecipes() {
+        OreDictionary.registerOre("wax", new ItemStack(FItems.INSTANCE.getHONEYCOMB()));
         StonecutterRecipes.INSTANCE.addRecipe(Ingredient.fromStacks(new ItemStack(DaemonicaBlocks.CHALK)), new ItemStack(DaemonicaBlocks.CHALK_STAIRS));
         StonecutterRecipes.INSTANCE.addRecipe(Ingredient.fromStacks(new ItemStack(DaemonicaBlocks.CHALK)), new ItemStack(DaemonicaBlocks.CHALK_SLAB, 2));
     }
