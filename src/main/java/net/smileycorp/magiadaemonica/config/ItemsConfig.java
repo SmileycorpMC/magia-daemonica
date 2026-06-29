27262b73e938464d594a9a7cbcdf9f16fe0ba438
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-06-29 12:45:40 +0100

    reorder item config

--- src/main/java/net/smileycorp/magiadaemonica/config/ItemsConfig.java
+++ src/main/java/net/smileycorp/magiadaemonica/config/ItemsConfig.java
@@ -63,30 +63,30 @@ public class ItemsConfig {
             calixPerpetuusMilk = config.getBoolean("canContainMilk", "calix perpetuus", true, "Can the calix perpetuus contain milk?");
             //chalk stick
             chalkDurability = config.getInt("durability", "chalk stick", 75, 0, Integer.MAX_VALUE, "How much durability do chalk sticks have?");
+            chalkEdible = config.getBoolean("edible", "chalk stick", true, "Can chalk sticks be eaten?");
             chalkHunger = config.getInt("hunger", "chalk stick", 1, 0, Integer.MAX_VALUE, "How much hunger does eating a chalk stick provide?");
             chalkSaturation = config.getFloat("saturation", "chalk stick", 0.8f, 0, Integer.MAX_VALUE, "How much saturation does eating a chalk stick provide?");
-            chalkEdible = config.getBoolean("edible", "chalk stick", true, "Can chalk sticks be eaten?");
             //facies excoriata
             faciesExcoriataEntityBlacklistStr = config.getStringList("entityBlacklist", "facies excoriata", new String[]{"minecraft:ender_dragon", "minecraft:wither"}, "Entities immune to the facies excoriata's petrify effect");
-            faciesExcoriataRange = config.getFloat("range", "facies excoriata", 32, 0, Float.MAX_VALUE, "How far away can the facies excoriata petrify mobs?");
             faciesExcoriataDuration = config.getInt("duration", "facies excoriata", 40, 0, Integer.MAX_VALUE, "How long does the facies excoriata's petrify effect work on mobs after you stop looking ar them?");
+            faciesExcoriataRange = config.getFloat("range", "facies excoriata", 32, 0, Float.MAX_VALUE, "How far away can the facies excoriata petrify mobs?");
             faciesExcoriataTickRate = config.getInt("tickRate", "facies excoriata", 10, 1, Integer.MAX_VALUE, "How often in ticks does the facies excoriata refresh it's petrify effect?");
             //lorica aculeta
-            loricaAculetaDamageReflection = config.getFloat("damageReflection", "lorica aculeta", 0.3f, 0, 1, "Percentage of damage the lorica aculeta reduces damage by (before armour and resistance) and reflects back to the attacker");
             loricaAculetaBleedTime = config.getInt("bleedTime", "lorica aculeta", 60, 0, Integer.MAX_VALUE, "How long does the lorica aculeta inflict bleed for after reflecting damage");
+            loricaAculetaDamageReflection = config.getFloat("damageReflection", "lorica aculeta", 0.3f, 0, 1, "Percentage of damage the lorica aculeta reduces damage by (before armour and resistance) and reflects back to the attacker");
             loricaAculetaRemovable = config.getBoolean("removable", "lorica aculeta", false, "Can the lorica aculeta be unequipped?");
             //oak bark
+            oakBarkEdible = config.getBoolean("edible", "oak bark", true, "Can oak bark be eaten?");
             oakBarkHunger = config.getInt("hunger", "oak bark", 1, 0, Integer.MAX_VALUE, "How much hunger does eating oak bark provide?");
             oakBarkSaturation = config.getFloat("saturation", "oak bark", 0.2f, 0, Integer.MAX_VALUE, "How much saturation does eating oak bark provide?");
-            oakBarkEdible = config.getBoolean("edible", "oak bark", true, "Can oak bark be eaten?");
             //pumpkin slice
             pumpkinSliceHunger = config.getInt("hunger", "pumpkin slice", 2, 0, Integer.MAX_VALUE, "How much hunger does eating a pumpkin slice provide?");
             pumpkinSliceSaturation = config.getFloat("saturation", "pumpkin slice", 1.2f, 0, Integer.MAX_VALUE, "How much saturation does eating a pumpkin slice provide?");
             //peccatum primordiale
+            peccatumPrimordialeDuration = config.getInt("duration", "peccatum primordiale", 200, 0, Integer.MAX_VALUE, "How many ticks do the peccatum primordiale's effects apply for?");
+            peccatumPrimordialeEffectBlacklistStr = config.getStringList("effectBlacklist", "peccatum primordiale", new String[]{"magiadaemonica:petrified", "magiadaemonica:tremor"}, "Potion effects the peccatum primordiale will not apply when eaten");
             peccatumPrimordialeHunger = config.getInt("hunger", "peccatum primordiale", 4, 0, Integer.MAX_VALUE, "How much hunger does eating the peccatum primordiale provide?");
             peccatumPrimordialeSaturation = config.getFloat("saturation", "peccatum primordiale", 2.4f, 0, Integer.MAX_VALUE, "How much saturation does eating the peccatum primordiale provide?");
-            peccatumPrimordialeEffectBlacklistStr = config.getStringList("effectBlacklist", "peccatum primordiale", new String[]{"magiadaemonica:petrified", "magiadaemonica:tremor"}, "Potion effects the peccatum primordiale will not apply when eaten");
-            peccatumPrimordialeDuration = config.getInt("duration", "peccatum primordiale", 200, 0, Integer.MAX_VALUE, "How many ticks do the peccatum primordiale's effects apply for?");
             //tallow
             tallowHunger = config.getInt("hunger", "tallow", 3, 0, Integer.MAX_VALUE, "How much hunger does eating tallow provide?");
             tallowSaturation = config.getFloat("saturation", "tallow", 0.4f, 0, Integer.MAX_VALUE, "How much saturation does eating tallow provide?");
